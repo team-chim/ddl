1dec7d0352a7232b1fca03dfdc5458efaf516ad1
Kris Kalavantavanich <kkalavantavanich@gmail.com>
2018-04-15 01:06:04 +0700

    Added database generation bash script

---
+++ import_sql.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Client Credentials Config file (WILL BE OVERWRITED IF EXISTS)
+
+CLIENT_CNF=client.cnf
+
+# Update client.cnf file 
+# $MYSQL_ROOT_PASSWORD is defined in mysql docker
+# otherwise use default root password as root
+
+echo "[client]" > $CLIENT_CNF
+echo "user=root" >> $CLIENT_CNF
+
+if [ $MYSQL_ROOT_PASSWORD ]
+then
+	echo "password=$MYSQL_ROOT_PASSWORD" >> $CLIENT_CNF
+else
+	echo "password=root" >> $CLIENT_CNF
+fi
+
+echo "Created mysql client config file as '$CLIENT_CNF'"
+
+# Loop through all *.sql files in lexical order
+
+COUNTER=0
+for f in *.sql
+do 
+	echo "Processing $f"
+	cat $f | mysql --defaults-file=$CLIENT_CNF
+	COUNTER=$((COUNTER + 1))
+done
+echo "Finished (processed $COUNTER files)"
