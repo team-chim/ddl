15069bb971f0b4efe0079c2140dfd254a8a4ecd5
munggumy <iknobkb@gmail.com>
2018-04-17 00:04:30 +0700

    Present Version

--- sql_sample_query/07_final_schedule.sql
+++ sql_sample_query/07_final_schedule.sql
@@ -1,4 +1,4 @@
-SELECT s.SubjectID, s.FinalDate
+SELECT s.SubjectID, s.FinalStartDatetime, s.FinalEndDatetime
 FROM `subject` s, studies stdy
 WHERE stdy.StudentID = '5830287921' AND
 	  s.SubjectID = stdy.SubjectID
--- sql_sample_query/07_midterm_schedule.sql
+++ sql_sample_query/07_midterm_schedule.sql
@@ -1,4 +1,4 @@
-SELECT s.SubjectID, s.MidtermDate
+SELECT s.SubjectID, s.MidtermStartDatetime, s.MidtermEndDatetime
 FROM `subject` s, studies stdy
 WHERE stdy.StudentID = '5830287921' AND
 	  s.SubjectID = stdy.SubjectID
