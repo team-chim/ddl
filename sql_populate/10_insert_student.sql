dbd3b240f19d5e1c8af7a3be8a5ebb2492a9dfc8
quack-ped <quack.ped@gmail.com>
2018-04-17 09:13:08 +0700

    Add files via upload

--- sql_populate/10_insert_student.sql
+++ sql_populate/10_insert_student.sql
@@ -3,4 +3,11 @@ USE rexchula;
 INSERT INTO student(`StudentID`, `FnameEN`, `MnameEN`, `LnameEN`, `FnameTH`, `MnameTH`, `LnameTH`, `StudentEmail`, `EnterYear`, `Adviser`, `Nation`, `MajorFaculty`, `MajorDepartment`)
 VALUES ('5730123421', 'Panich', 'K', 'Wangwiwat', 'พณิช', 'เค', 'วังวิวัฒน์', 'panich.w@gmail.com', 2014, '0123456789', 'Thai', 21, 10),
        ('5732831021', 'Jirachai', NULL, 'Kochakrit', 'จิรชัย', NULL, 'คชกฤษ', 'jirachai.k@gmail.com', 2014, '0123456789', 'Thai', 21, 10),
-       ('5731150221', 'Sirisopha', NULL, 'Thamwattanakan', 'ศิริโสภา', NULL, 'ธรรมวัฒนากาญจน์', 'sirisopha.th@gmail.com', 2014, '0123456789', 'Thai', 21, 10);
+       ('5731150221', 'Sirisopha', NULL, 'Thamwattanakan', 'ศิริโสภา', NULL, 'ธรรมวัฒนากาญจน์', 'sirisopha.th@gmail.com', 2014, '0123456789', 'Thai', 21, 10),
+	   ('5704433221','Richard','R','Rich','ริชาร์ด','อาร์','ริช','richRichard@gmail.com',2014,'6789012345','Canadian',21,10),
+	   ('5702229921','Tanawat',NULL,'Wongdee','ธนวัฒน์',NULL,'วงดี','wongdee.t@gmail.com',2014,'5678901234','Thai',21,10),
+	   ('5701132221','Kanlaya',NULL,'Kaewdkloaw','กัลลยา',NULL,'แก้วกล้าว','kanlawy.k@gmail.com',2014,'4567890123','Thai',21,10),
+	   ('5830244121','Maitree',NULL,'Chanpetch','ไมตรี',NULL,'จันเพชร','mtree.c@gmail.com',2015,'4567890123','Thai',21,10),
+	   ('5630297121','Benja',NULL,'Buree','เบญจ',NULL,'บุรี','benjy.bubu@gmail.com',2013,'1234567890','Thai',21,10),
+	   ('5732987621','Napat',NULL,'Siangsomboon','ณภัทร',NULL,'เสียงสมบุญ','napat.s@gmail.com',2014,'1234567890','Thai',21,10);
+	   
