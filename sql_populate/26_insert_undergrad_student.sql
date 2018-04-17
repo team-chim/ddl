e61d5f890c7c9b06f3e944d015fb1b9d61558727
quack-ped <quack.ped@gmail.com>
2018-04-17 09:24:40 +0700

    Add files via upload

--- sql_populate/26_insert_undergrad_student.sql
+++ sql_populate/26_insert_undergrad_student.sql
@@ -3,3 +3,5 @@ USE rexchula;
 INSERT INTO undergrad_student (`StudentID`, `MinorFaculty`, `MinorDepartment`)
 VALUES ('5730123421', NULL, NULL),
        ('5731150221', NULL, NULL);
+	   ('5702229921', NULL, NULL),
+	   ('5701132221', NULL, NULL);
