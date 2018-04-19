796c83cf210f6932fc02e224266ab7f2bd7abbad
Kris Kalavantavanich <kkalavantavanich@gmail.com>
2018-04-19 09:56:11 +0700

    Added more subjects

--- sql_populate/24_insert_has_fee.sql
+++ sql_populate/24_insert_has_fee.sql
@@ -1,7 +1,11 @@
 USE rexchula;
 
 INSERT INTO has_fee (`FacultyGroupID`, `IsThai`, `EntryYear`, `GradFee`, `UndergradFee`)
-VALUES (1, TRUE, 2014, 25000, 21000),
+VALUES (1, TRUE, 2013, 25000, 21000),
+       (2, TRUE, 2013, 18000, 22000),
+       (1, TRUE, 2014, 25000, 21000),
        (2, TRUE, 2014, 18000, 22000),
        (1, TRUE, 2015, 25000, 21000),
-       (2, TRUE, 2015, 18000, 22000);
+       (2, TRUE, 2015, 18000, 22000),
+       (1, TRUE, 2016, 25000, 21000),
+       (2, TRUE, 2016, 18000, 22000);
