0bbd9b06c743133a52e02d8fcd5c438816f82216
munggumy <iknobkb@gmail.com>
2018-04-30 18:12:11 +0700

    fix insert subject

--- sql_sample_query/insert/105_create_procedure_insert_subject.sql
+++ sql_sample_query/insert/105_create_procedure_insert_subject.sql
@@ -4,8 +4,8 @@ DROP PROCEDURE IF EXISTS insert_subject;
 
 DELIMITER $$
 
-CREATE PROCEDURE insert_subject(IN SubjectID CHAR(7), IN NameAbv VARCHAR(128), IN NameEN VARCHAR(128), IN NameTH VARCHAR(128))
+CREATE PROCEDURE insert_subject(IN SubjectID CHAR(7), IN NameAbv VARCHAR(128), IN NameEN VARCHAR(128), IN NameTH VARCHAR(128), IN Credit INT UNSIGNED)
 BEGIN
-	INSERT INTO `subject`(SubjectID, NameAbv, NameEN, NameTH) VALUES(SubjectID, NameAbv, NameEN, NameTH);
+	INSERT INTO `subject`(SubjectID, NameAbv, NameEN, NameTH, Credit) VALUES(SubjectID, NameAbv, NameEN, NameTH, Credit);
 END$$
 DELIMITER ;
