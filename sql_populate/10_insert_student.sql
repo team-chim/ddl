2369e82d0eda38fb34659cff83ed4db7112a3675
quack-ped <quack.ped@gmail.com>
2018-05-20 12:55:53 +0700

    Update 10_insert_student.sql
    Editing format.

--- sql_populate/10_insert_student.sql
+++ sql_populate/10_insert_student.sql
@@ -1,13 +1,13 @@
 USE rexchula;
 
 INSERT INTO student(`StudentID`, `FnameEN`, `MnameEN`, `LnameEN`, `FnameTH`, `MnameTH`, `LnameTH`, `StudentEmail`, `Adviser`, `Nation`, `MajorFaculty`, `MajorDepartment`)
-VALUES ('5730123421', 'Panich', 'K', 'Wangwiwat', 'พณิช', 'เค', 'วังวิวัฒน์', 'panich.w@gmail.com', '0010003176', 'Thai', 21, 10),
-       ('5732831021', 'Jirachai', NULL, 'Kochakrit', 'จิรชัย', NULL, 'คชกฤษ', 'jirachai.k@gmail.com', '0010003176', 'Thai', 21, 10),
-       ('5731150221', 'Sirisopha', NULL, 'Thamwattanakan', 'ศิริโสภา', NULL, 'ธรรมวัฒนากาญจน์', 'sirisopha.th@gmail.com', '0010003176', 'Thai', 21, 10),
-	   ('5734433221','Richard','R','Rich','ริชาร์ด','อาร์','ริช','richRichard@gmail.com','0010013658','Canadian',21,10),
-	   ('5732229921','Tanawat',NULL,'Wongdee','ธนวัฒน์',NULL,'วงดี','wongdee.t@gmail.com','0010013658','Thai',21,10),
-	   ('5731132221','Kanlaya',NULL,'Kaewdkloaw','กัลลยา',NULL,'แก้วกล้าว','kanlawy.k@gmail.com','0000040461','Thai',21,10),
-	   ('5830244121','Maitree',NULL,'Chanpetch','ไมตรี',NULL,'จันเพชร','mtree.c@gmail.com','0000040461','Thai',21,10),
-	   ('5670297121','Benja',NULL,'Buree','เบญจ',NULL,'บุรี','benjy.bubu@gmail.com','0000034157','Thai',21,10),
-	   ('5732987621','Napat',NULL,'Siangsomboon','ณภัทร',NULL,'เสียงสมบุญ','napat.s@gmail.com','0000034157','Thai',21,10);
+VALUES	('5730123421', 'Panich', 'K', 'Wangwiwat', 'พณิช', 'เค', 'วังวิวัฒน์', 'panich.w@gmail.com', '0010003176', 'Thai', 21, 10),
+	('5732831021', 'Jirachai', NULL, 'Kochakrit', 'จิรชัย', NULL, 'คชกฤษ', 'jirachai.k@gmail.com', '0010003176', 'Thai', 21, 10),
+	('5731150221', 'Sirisopha', NULL, 'Thamwattanakan', 'ศิริโสภา', NULL, 'ธรรมวัฒนากาญจน์', 'sirisopha.th@gmail.com', '0010003176', 'Thai', 21, 10),
+	('5734433221','Richard','R','Rich','ริชาร์ด','อาร์','ริช','richRichard@gmail.com','0010013658','Canadian',21,10),
+	('5732229921','Tanawat',NULL,'Wongdee','ธนวัฒน์',NULL,'วงดี','wongdee.t@gmail.com','0010013658','Thai',21,10),
+	('5731132221','Kanlaya',NULL,'Kaewdkloaw','กัลลยา',NULL,'แก้วกล้าว','kanlawy.k@gmail.com','0000040461','Thai',21,10),
+	('5830244121','Maitree',NULL,'Chanpetch','ไมตรี',NULL,'จันเพชร','mtree.c@gmail.com','0000040461','Thai',21,10),
+	('5670297121','Benja',NULL,'Buree','เบญจ',NULL,'บุรี','benjy.bubu@gmail.com','0000034157','Thai',21,10),
+	('5732987621','Napat',NULL,'Siangsomboon','ณภัทร',NULL,'เสียงสมบุญ','napat.s@gmail.com','0000034157','Thai',21,10);
 	   
