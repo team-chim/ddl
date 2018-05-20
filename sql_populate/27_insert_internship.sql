47a77c4f6617ee121e1feab830e333cca6d7e472
quack-ped <quack.ped@gmail.com>
2018-05-20 13:02:14 +0700

    Update 27_insert_internship.sql
    Edit format.

--- sql_populate/27_insert_internship.sql
+++ sql_populate/27_insert_internship.sql
@@ -1,8 +1,8 @@
 USE rexchula;
 
 INSERT INTO internship (`StudentID`, `Year`, `CompanyID`, `BranchName`, `Status`, `StartDate`, `EndDate`, `PositionNameEN`, `PositionNameTH`, `Comment`, `Rating`, `OfferID`)
-VALUES ('5730123421', 2016, 2, 1, 'Pending', '2017-06-01', '2017-07-30', 'Frontend Developer', NULL, 'enjoyed to working here, good food too', 9, NULL),
-	   ('5701132221', 2016, 2, 1, 'Completed', '2016-06-01', '2016-07-30', 'Frontend Developer', NULL, 'Good food ii', 8, NULL),
-	   ('5702229921', 2016, 2, 1, 'Completed', '2016-06-01', '2016-07-30', 'Backend Developer', NULL, 'good company. good workspace', 10, NULL),
-       ('5731150221', 2016, 2, 1, 'Pending', '2017-06-01', '2017-07-30', 'Backend Developer', NULL, 'very good company. very comfortable workspace', 8, NULL);
+VALUES	('5730123421', 2016, 2, 1, 'Pending', '2017-06-01', '2017-07-30', 'Frontend Developer', NULL, 'enjoyed to working here, good food too', 9, NULL),
+	('5701132221', 2016, 2, 1, 'Completed', '2016-06-01', '2016-07-30', 'Frontend Developer', NULL, 'Good food ii', 8, NULL),
+	('5702229921', 2016, 2, 1, 'Completed', '2016-06-01', '2016-07-30', 'Backend Developer', NULL, 'good company. good workspace', 10, NULL),
+	('5731150221', 2016, 2, 1, 'Pending', '2017-06-01', '2017-07-30', 'Backend Developer', NULL, 'very good company. very comfortable workspace', 8, NULL);
 	   
