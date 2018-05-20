63a4adc1fb3f89660e4bd5afe6435c04667f6902
quack-ped <quack.ped@gmail.com>
2018-05-20 11:21:02 +0700

    Update 03_insert_company.sql
    adding more population.

--- sql_populate/03_insert_company.sql
+++ sql_populate/03_insert_company.sql
@@ -3,4 +3,8 @@ use rexchula;
 INSERT INTO company (`NameEN`, `NameTH`)
 VALUES ('SET','ตลาดหลักทรัพย์แห่งประเทศไทย'),
  ('Wongnai','วงใน'),
- ('Google','กูเกิ้ล');
+ ('Google','กูเกิ้ล'),
+ ('Agoda','อโกด้า'),
+ ('SCG','ปูนซิเมนต์ไทย'),
+ ('Toyota','โตโยต้า');
+ 
