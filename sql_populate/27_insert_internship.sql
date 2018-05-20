8f35463abe5a7d8e0bc7e6615398f72ce8f0f22b
quack-ped <quack.ped@gmail.com>
2018-05-20 20:34:43 +0700

    Update 27_insert_internship.sql
    Add more internship

--- sql_populate/27_insert_internship.sql
+++ sql_populate/27_insert_internship.sql
@@ -8,5 +8,8 @@ VALUES	('5730123421', 2016, 2, 1, 'Completed', '2017-06-01', '2017-07-30', 'Fron
 	('5732831021', 2016, 3, 1, 'Completed', '2017-06-01', '2017-08-05', 'Frontend Developer', NULL, 'Challenging, good environment, the company take cares their employees.', 9, NULL),
 	('5732987621', 2016, 4, 'Bangkok', 'Completed', '2016-06-01', '2016-07-30', 'Backend Developer', NULL, '-', 8, 1),
 	('5734433221', 2016, 4, 'Bangkok', 'Completed', '2017-06-01', '2017-07-30', 'Backend Developer', NULL, 'great team, great office', 8, 1),
+	('5742117922', 2016, 2, 1, 'Completed', '2017-06-01', '2017-07-30', 'Content Editor(Food Writer)', NULL, 'great food haha', 10, NULL),
+	('5740037122', 2016, 4, 'Bangkok', 'Completed', '2017-06-01', '2017-07-30', 'Content Analyst', NULL, 'Love working here', 9, NULL),
+	('5732671526', 2016, 4, 'Bangkok', 'Completed', '2017-06-01', '2017-07-30', 'Market Manager', NULL, 'Little bit hard but i love challenge', 8, NULL),
 	('5830244121', 2017, 4, 'Bangkok', 'Approved', '2018-06-03', '2018-08-03', 'Backend Developer', NULL, NULL, NULL, 1);
 	   
