9c81d659d1e2850ff91fd8798f6d57b63738940b
quack-ped <quack.ped@gmail.com>
2018-05-20 11:59:38 +0700

    Update 18_insert_main_branch.sql
    adding more population.

--- sql_populate/18_insert_main_branch.sql
+++ sql_populate/18_insert_main_branch.sql
@@ -3,5 +3,8 @@ USE rexchula;
 INSERT INTO main_branch (`CompanyID`, `BranchName`)
 VALUES (1, '-'),
        (2, '1'),
-       (3, '1');
+       (3, '1'),
+       (4, 'Bangkok'),
+       (5, '-'),
+       (6, '-');
 
