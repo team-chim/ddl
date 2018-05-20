4604475eeb0244d8a8409c37bd4b005ae6244b4d
quack-ped <quack.ped@gmail.com>
2018-05-20 21:18:19 +0700

    Update 12_insert_internship_offer.sql
    Add more internship_offers.

--- sql_populate/12_insert_internship_offer.sql
+++ sql_populate/12_insert_internship_offer.sql
@@ -11,4 +11,10 @@ VALUES (1, 2016, 'Backend Developer', NULL, 'Implement backend of our applicatio
        (4, 2017, 'Frontend Developer', NULL, 'Implement company website', NULL, 3, 'Javascript, React,', 2.75, 750, NULL, '2018-03-31'),
        (5, 2017, 'Software Engineering Internship', NULL, 'Software Engineering Interns.', NULL, 2, NULL, NULL, 400, NULL, '2017-12-20'),
        (5, 2017, 'Software Developer', NULL, 'Impliment new software for company.', NULL, 2, NULL, NULL, 400, NULL, '2017-12-20'),
-       (6, 2017, 'Software Developer', NULL, 'Create new software to support customers.', NULL, 3, NULL, NULL, 300, NULL, '2017-12-15');
+       (6, 2017, 'Software Developer', NULL, 'Create new software to support customers.', NULL, 3, NULL, NULL, 300, NULL, '2017-12-15'),
+       (1, 2018, 'Backend Developer', NULL, 'Implement backend of our application', NULL, 2, 'Javascript, Docker, NodeJS', 2.75, 500, NULL, '2017-12-31'),
+       (2, 2018, 'Backend Developer', NULL, 'Implement backend of our application', NULL, 3, 'Javascript, Docker, NodeJS', 2.75, 500, NULL, '2018-01-15'),
+       (4, 2018, 'Backend Developer', NULL, 'Implement backend of our application', NULL, 3, 'Javascript, Docker, NodeJS', 2.75, 750, NULL, '2018-02-27'),
+       (4, 2018, 'Data Analyst', NULL, 'Create insight of our clients and deliver new features.', NULL, 3, 'Python, Spark, Keras', 2.75, 750, NULL, '2018-02-27'),
+       (4, 2018, 'Frontend Developer', NULL, 'Implement company website', NULL, 3, 'Javascript, React,', 2.75, 750, NULL, '2018-02-27'),
+       (5, 2018, 'Software Engineering Internship', NULL, 'Software Engineering Interns.', NULL, 2, NULL, NULL, 400, NULL, '2017-12-01');
