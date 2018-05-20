073c0609a2524ed4423638f7096f7bae1380b619
Kris Kalavantavanich <kkalavantavanich@gmail.com>
2018-05-20 17:45:04 +0700

    Changed department head from 2101 to 2110

--- sql_populate/17_insert_managementship.sql
+++ sql_populate/17_insert_managementship.sql
@@ -1,5 +1,5 @@
 USE rexchula;
 
 INSERT INTO managementship(`TeacherID`, `FacultyID`, `DepartmentID`, `StartDate`, `EndDate`)
-VALUES ('5678901234', 21, 1, '2014-05-01', NULL),
+VALUES ('5678901234', 21, 10, '2014-05-01', NULL),
        ('7890123456', 22, 1, '2014-05-01', NULL);
