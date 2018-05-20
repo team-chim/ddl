38617aa50f99052c1fdfd6362cfdd3a870e12d74
quack-ped <quack.ped@gmail.com>
2018-05-20 12:17:06 +0700

    Update 22_insert_company_industries.sql
    adding more population.

--- sql_populate/22_insert_company_industries.sql
+++ sql_populate/22_insert_company_industries.sql
@@ -11,3 +11,15 @@ VALUES (2,'Technology'),
 INSERT INTO company_industries (`CompanyID`, `Industry`)
 VALUES (3,'Technology'),
        (3,'Search-Engine');
+       
+INSERT INTO company_industries (`CompanyID`, `Industry`)
+VALUES (4,'Technology'),
+       (4,'Travel agency');
+
+INSERT INTO company_industries (`CompanyID`, `Industry`)
+VALUES	(5,'Chemical'),
+	(5,'Construction Material'),
+	(5,'Packaing');
+		
+INSERT INTO company_industries (`CompanyID`, `Industry`)
+VALUES	(6,'Car manufacturing');
