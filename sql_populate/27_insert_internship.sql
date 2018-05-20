c9a4054fbe527f17743edcbc32c12206df506649
quack-ped <quack.ped@gmail.com>
2018-05-20 13:47:46 +0700

    Update 27_insert_internship.sql
    Adding internships that didn't take and take company offers in 2016.

--- sql_populate/27_insert_internship.sql
+++ sql_populate/27_insert_internship.sql
@@ -4,5 +4,8 @@ INSERT INTO internship (`StudentID`, `Year`, `CompanyID`, `BranchName`, `Status`
 VALUES	('5730123421', 2016, 2, 1, 'Pending', '2017-06-01', '2017-07-30', 'Frontend Developer', NULL, 'enjoyed to working here, good food too', 9, NULL),
 	('5731132221', 2016, 1, '-', 'Completed', '2016-06-01', '2016-07-30', 'Frontend Developer', NULL, 'Good food ii', 8, 1),
 	('5732229921', 2016, 2, 1, 'Completed', '2016-06-01', '2016-07-30', 'Backend Developer', NULL, 'good company. good workspace', 10, NULL),
-	('5731150221', 2016, 4, 'Bangkok', 'Pending', '2017-06-01', '2017-07-30', 'Backend Developer', NULL, 'very good company. very comfortable workspace', 8, 1);
+	('5731150221', 2016, 4, 'Bangkok', 'Pending', '2017-06-01', '2017-07-30', 'Backend Developer', NULL, 'very good company. very comfortable workspace', 8, 1),
+	('5732831021', 2016, 3, 1, 'Completed', '2017-06-01', '2017-08-05', 'Frontend Developer', NULL, 'Challenging, good environment, the company take cares their employees.', 9, NULL),
+	('5732987621', 2016, 4, 'Bangkok', 'Completed', '2016-06-01', '2016-07-30', 'Backend Developer', NULL, '-', 8, 1),
+	('5734433221', 2016, 4, 'Bangkok', 'Completed', '2017-06-01', '2017-07-30', 'Backend Developer', NULL, 'great team, great office', 8, 1);
 	   
