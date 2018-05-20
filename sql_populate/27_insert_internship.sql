a5841086d8acd495095ac114235917b8c49983be
quack-ped <quack.ped@gmail.com>
2018-05-20 14:22:22 +0700

    Update 27_insert_internship.sql
    Edit status of internship that already passed to Completed and adds an approved internship

--- sql_populate/27_insert_internship.sql
+++ sql_populate/27_insert_internship.sql
@@ -1,11 +1,12 @@
 USE rexchula;
 
 INSERT INTO internship (`StudentID`, `Year`, `CompanyID`, `BranchName`, `Status`, `StartDate`, `EndDate`, `PositionNameEN`, `PositionNameTH`, `Comment`, `Rating`, `OfferID`)
-VALUES	('5730123421', 2016, 2, 1, 'Pending', '2017-06-01', '2017-07-30', 'Frontend Developer', NULL, 'enjoyed to working here, good food too', 9, NULL),
+VALUES	('5730123421', 2016, 2, 1, 'Completed', '2017-06-01', '2017-07-30', 'Frontend Developer', NULL, 'enjoyed to working here, good food too', 9, NULL),
 	('5731132221', 2016, 1, '-', 'Completed', '2016-06-01', '2016-07-30', 'Frontend Developer', NULL, 'Good food ii', 8, 1),
 	('5732229921', 2016, 2, 1, 'Completed', '2016-06-01', '2016-07-30', 'Backend Developer', NULL, 'good company. good workspace', 10, NULL),
-	('5731150221', 2016, 4, 'Bangkok', 'Pending', '2017-06-01', '2017-07-30', 'Backend Developer', NULL, 'very good company. very comfortable workspace', 8, 1),
+	('5731150221', 2016, 4, 'Bangkok', 'Completed', '2017-06-01', '2017-07-30', 'Backend Developer', NULL, 'very good company. very comfortable workspace', 8, 1),
 	('5732831021', 2016, 3, 1, 'Completed', '2017-06-01', '2017-08-05', 'Frontend Developer', NULL, 'Challenging, good environment, the company take cares their employees.', 9, NULL),
 	('5732987621', 2016, 4, 'Bangkok', 'Completed', '2016-06-01', '2016-07-30', 'Backend Developer', NULL, '-', 8, 1),
-	('5734433221', 2016, 4, 'Bangkok', 'Completed', '2017-06-01', '2017-07-30', 'Backend Developer', NULL, 'great team, great office', 8, 1);
+	('5734433221', 2016, 4, 'Bangkok', 'Completed', '2017-06-01', '2017-07-30', 'Backend Developer', NULL, 'great team, great office', 8, 1),
+	('5830244121', 2017, 4, 'Bangkok', 'Approved', '2018-06-03', '2017-08-03', 'Backend Developer', NULL, NULL, NULL, 1);
 	   
