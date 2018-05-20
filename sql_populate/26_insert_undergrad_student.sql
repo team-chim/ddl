f0edf1fd3a519bd5c7b6bd10be092201558f23ff
quack-ped <quack.ped@gmail.com>
2018-05-20 12:45:28 +0700

    Update 26_insert_undergrad_student.sql
    adding more population.

--- sql_populate/26_insert_undergrad_student.sql
+++ sql_populate/26_insert_undergrad_student.sql
@@ -4,4 +4,8 @@ INSERT INTO undergrad_student (`StudentID`, `MinorFaculty`, `MinorDepartment`)
 VALUES ('5730123421', NULL, NULL),
        ('5731150221', NULL, NULL),
        ('5702229921', NULL, NULL),
-       ('5701132221', NULL, NULL);
+       ('5701132221', NULL, NULL),
+       ('5704433221', NULL, NULL),
+       ('5732831021', NULL, NULL),
+       ('5732987621', NULL, NULL),
+       ('5830244121', NULL, NULL);
