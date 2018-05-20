3f0c84a80c2d0f68c0a7827ecf2d1679e9ea4165
quack-ped <quack.ped@gmail.com>
2018-05-20 15:08:42 +0700

    Update 27_insert_internship.sql
    Edit wrong query.

--- sql_populate/27_insert_internship.sql
+++ sql_populate/27_insert_internship.sql
@@ -8,5 +8,5 @@ VALUES	('5730123421', 2016, 2, 1, 'Completed', '2017-06-01', '2017-07-30', 'Fron
 	('5732831021', 2016, 3, 1, 'Completed', '2017-06-01', '2017-08-05', 'Frontend Developer', NULL, 'Challenging, good environment, the company take cares their employees.', 9, NULL),
 	('5732987621', 2016, 4, 'Bangkok', 'Completed', '2016-06-01', '2016-07-30', 'Backend Developer', NULL, '-', 8, 1),
 	('5734433221', 2016, 4, 'Bangkok', 'Completed', '2017-06-01', '2017-07-30', 'Backend Developer', NULL, 'great team, great office', 8, 1),
-	('5830244121', 2017, 4, 'Bangkok', 'Approved', '2018-06-03', '2017-08-03', 'Backend Developer', NULL, NULL, NULL, 1);
+	('5830244121', 2017, 4, 'Bangkok', 'Approved', '2018-06-03', '2018-08-03', 'Backend Developer', NULL, NULL, NULL, 1);
 	   
