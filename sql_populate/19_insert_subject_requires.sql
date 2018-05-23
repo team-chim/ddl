cb64e2319e6d97539a68ed0e7294b0252974b1c3
quack-ped <quack.ped@gmail.com>
2018-05-23 16:27:44 +0700

    Update 19_insert_subject_requires.sql
    Add more subject requirements.

--- sql_populate/19_insert_subject_requires.sql
+++ sql_populate/19_insert_subject_requires.sql
@@ -1,4 +1,7 @@
 USE rexchula;
 
 INSERT INTO subject_requires (`SubjectID`, `ReqSubjectID`)
-    VALUES ('2110332', '2110316');
+    VALUES  ('2110332', '2110316'),
+            ('2110211', '2110101'),
+	        ('2110265', '2110251'),
+            ('2101202', '2103213');
